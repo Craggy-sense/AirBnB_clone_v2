abffff5c1b645cf139a5d3d356c16305a5201891
Craggy-sense <memusirobi@gmail.com>
2023-04-28 11:32:44 +0000

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_holberton():
+def index():
     """Returns a string at the root route"""
     return 'Hello HBNB!'
 
@@ -26,4 +26,4 @@ def cisfun(text):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
